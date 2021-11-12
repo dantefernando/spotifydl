ae22b4bc8df998d2b9583db16a6dddedfc6c9acc
dantefernando@github.com <coolguymctmg@gmail.com>
2021-11-12 12:30:24 +0100

    input edits

--- spotifydl.py
+++ spotifydl.py
@@ -3,6 +3,10 @@
 # Download an entire Spotify playlist using YouTube mp3s
 # https://stackoverflow.com/questions/27473526/download-only-audio-from-youtube-video-using-youtube-dl-in-python-script
 
+
+# pytube module downloading getting streams of youtube
+# youtubesearchpython module
+
 from __future__ import unicode_literals
 import youtube_dl
 
@@ -21,17 +25,19 @@ def downloader():
     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
         ydl.download(['https://youtu.be/CiyamJhSbBg'])
 
+
 def get_song():
     print("Enter a YouTube Link to Download.")
     while True:
         try:
             song = input("Link: ")
         except ValueError:
-            print("Enter a ")
+            print("Please enter a valid URL link, try again...")
 
 
 def main():
-    print("Hello World")
+    get_song()
+
 
 if __name__ == "__main__":
     main()
