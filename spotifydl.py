df8d1e89ad1237c9567b94d233bef29223266509
dantefernando <coolguymctmg@gmail.com>
2021-11-30 00:04:53 +0100

    Added music/ dir, checks existing songs in dir
    Songs now get downloaded into the music/ dir, if the user presses ctrl-c the program will exit without any errors, if songs already exist in the music/ dir user will be asked to redownload or skip, program loops to keep asking for songs to download

--- spotifydl.py
+++ spotifydl.py
@@ -1,15 +1,27 @@
 #!/bin/python3
 
+# Dante Fernando 2021
 # Download an entire Spotify playlist using YouTube mp3s
+
 # https://stackoverflow.com/questions/27473526/download-only-audio-from-youtube-video-using-youtube-dl-in-python-script
 
-# TODO:
-# - Exceptions for having no internet connection
-# - Put all downloaded music into a directory called 'music'
+# - pytube module downloading getting streams of youtube
+
+# TODO High Priority
 # - Add support for adding a spotify playlist and getting a list of
 # songs from the playlist
+# - Make folders inside music/ with time and date for different sessions
 
-# pytube module downloading getting streams of youtube
+# TODO Medium Priority
+# - Add a settings menu to choose from different defaults
+# when downloading music. E.g. default to choose to download song
+# with most views instead of the first index
+
+# TODO Low Priority
+# - Exceptions for having no internet connection
+# - Delete partly downloaded music
+
+
 
 # from __future__ import unicode_literals
 from youtubesearchpython import VideosSearch
@@ -17,21 +29,77 @@ import youtube_dl
 import os
 
 
+# Return True if the song exists in music/ , otherwise return False
+def songExists(title, files):
+    if f"{title}.mp3" in files:  # File is already downloaded
+        return True
+    else:  # File doesn't exist / hasn't been downloaded yet
+        return False
+
+
+# Make the music/ directory if it doesn't already exist
+def makeMusicDir():
+    cwd = os.getcwd()  # Get cwd
+    path = os.path.join(cwd, "music")  # Path object
+
+    # Try and make the music/ dir assuming it doesn't exist
+    try:
+        os.mkdir(path)  # Make the in the working directory
+        files = []
+    except FileExistsError:  # music/ exists already
+        files = os.listdir(path)  # list of files in music/
+
+    return path, files
+
+
 # Download song using provided link
-def downloader(link):
-    ydl_opts = {
-        'format': 'bestaudio/best',
-        'outtmpl': '%(title)s.%(ext)s',
-        'postprocessors': [{
-        'key': 'FFmpegExtractAudio',
-        'preferredcodec': 'mp3',
-        'preferredquality': '192',
-        }],
-    }
-    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-        ydl.download([link])
+# Kwargs: redownload=BOOL
+def downloader(songData, **kwargs):
+
+    # Sets values using kwargs to determine
+    # whether or not to redownload the song:
+    redownload = kwargs.get("redownload")  # Either True or False
+
+    # Extract the YouTube video's attributes from songData
+    link = songData['link']
+    title = songData['title']
+
+    path, files = makeMusicDir()  # Make the music/ dir
+
+    # Song doesn't exist, download the song or the user wants to redownload the song
+    if not songExists(title, files) or redownload == True:
+
+        ydl_opts = {
+            'format': 'bestaudio/best',
+            'outtmpl': f'{path}/%(title)s.%(ext)s',
+            'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+            'preferredquality': '192',
+            }],
+        }
+        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+            ydl.download([link])
+            print("\n\n")
+
+    else:  # Song exists in the folder
+        print(f"\"{title}\" has been downloaded already.")
+        while True:  # Loop until valid input has been receieved
+            inp = input("(Default: Skip)\n(S)kip or (R)edownload song: ").lower()
+
+            if inp == "" or inp == "s":  # User chose to skip download
+                print("Song skipped!!\n\n")
+                break
+            elif inp == "r":  # User chose to redownload the song
+                downloader(songData, redownload=True)
+                break
+            else:  # Invalid input
+                print("Please enter a valid input, try again...\n")
 
 
+# Display the songs from the YouTube search
+# results in a readable format with title,
+# channel, duration and views
 def displaySongs(stripped_results):
     print("\n")
     for index, song in enumerate(stripped_results):
@@ -57,17 +125,19 @@ def getSongData(stripped_results):
         try:
             if inp == "":  # Default = 1 so 0th song
                 songData = stripped_results[0]
+                print("\n")
                 return songData
             else:
                 inp = int(inp)
                 numSongs = len(stripped_results)
                 if inp == 0 or inp > numSongs:
-                    print("Please enter a valid index number.\nTry again...")
+                    print(f"Please enter a valid index number between 1-{len(stripped_results)}.\nTry again...\n")
                 else:  # Valid
                     songData = stripped_results[inp-1]
+                    print("\n")
                     return songData
         except ValueError:
-            print("Please enter a valid index, must be a number.\nTry again...")
+            print("Please enter a valid index, must be an integer.\nTry again...\n")
 
 
 # Get YouTube Search Results on YouTube
@@ -100,10 +170,10 @@ def getUserInput():
 
         # Validation for song name
         if len(song_name) == 0:
-            print("You need to enter a song title.\n Try again...")
+            print("You need to enter a song title.\nTry again...\n")
         else:  # Validation for search_limit
-            print("Enter the number of songs to show from YouTube: ")
-            print("E.g. Entering '3' will only show the first 3 songs from YouTube.")
+            print("\nEnter the number of songs to show from YouTube (Max: 20). ")
+            print("E.g. Entering \'3\' will only show the first 3 songs from YouTube.")
             print("(Default: 5)")
             while True:
                 search_limit = input("Number of songs to show: ")
@@ -114,23 +184,24 @@ def getUserInput():
                     else:
                         search_limit = int(search_limit)
                         if search_limit == 0 or search_limit > 20:
-                            print("Enter a search result value between 1 and 20.\nTry again...")
+                            print("Enter a search result value between 1 and 20.\nTry again...\n")
                         else:  # Valid
                             return song_name, search_limit
                 except ValueError:
-                    print("Please input a valid number of search results to show.\nTry again...")
+                    print("Please input a valid number of search results to show.\nTry again...\n")
 
 
 def main():
-    song_name , search_limit = getUserInput()
-    stripped_results = getResults(song_name, search_limit)
-    songData = getSongData(stripped_results)
-    print(songData)
-    link = songData['link']
-    downloader(link)
+    try:
+        while True:  # Loop to keep downloading songs
+            song_name , search_limit = getUserInput()
+            stripped_results = getResults(song_name, search_limit)
+            songData = getSongData(stripped_results)
+            downloader(songData)
+    except KeyboardInterrupt:  # User hits ctrl-c on keyboard 
+        print("\n\nExiting program...")
 
 
 if __name__ == "__main__":
     main()
-
 
